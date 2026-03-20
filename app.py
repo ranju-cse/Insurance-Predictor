0c5681ba0279cfed4e53dda234e989dc48e6b039
Ranju Yadav <ry526962@gmail.com>
2026-03-20 17:00:19 +0530

    Added Main File

--- app.py
+++ app.py
@@ -1,89 +1,43 @@
 import pandas as pd
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-from pydantic import BaseModel,Field,computed_field
-from typing import Literal,Annotated
-import pickle
+from schema.user_input import User
+from schema.prediction_response import PredictionResponse
+from Model.predict import predict_output,model,MODEL_VERSION
 
-#import the ml model
-with open('model.pkl' ,'rb') as f:
-    model=pickle.load(f)
 
 app=FastAPI()
 
 
-Tier_1=["Mumbai","Delhi","Banglore","Chennai","Kolkata","Hyderabad","Pune"]
-Tier_2=["Jaipur","Chandigarh","Indore","Lucknow","Ranchi"]
-#Pydantic model to validate incoming data
-class User(BaseModel):
-    age:Annotated[int,Field(...,gt=0,lt=120,description='Age of user')]
-    weight:Annotated[float,Field(...,gt=0,description='Weight of user')]
-    height:Annotated[float,Field(...,gt=0,lt=2.5,description='Height of user')]
-    income_lpa:Annotated[float,Field(...,gt=0,description='Annual Salary of user in lpa')]
-    smoker:Annotated[bool,Field(...,description='Is user smoke')]
-    city:Annotated[str,Field(...,description='City of user')]
-    occupation:Annotated[Literal['retired','freelancer','student','government_job','buisness_owner','unemployed','private_job'],Field(...,description='Occupation of user')]
-
-
-    @computed_field
-    @property
-    def bmi(self)-> float:
-        return round(self.weight/(self.height**2),2)
-    
-    #Lifestyle Risk
-    @computed_field
-    @property
-    def lifestyle_risk(self)->str:
-        if self.smoker or self.bmi > 30:
-         return "high"
-        elif self.bmi > 27:
-         return "medium"
-        else:
-          return "low"
-        
-    #Age Group
-    @computed_field
-    @property
-    def age_group(self)->str:
-        if self.age < 25:
-            return 'young'
-        elif  self.age>45:
-            return "adult"
-        elif self.age> 60:
-         return "middle_aged"
-        else:
-            return "senior"
-     
-        
-    #City Tier
-    @computed_field
-    @property
-    def city_tier(self)->str:
-        if self.city in Tier_1:
-            return 1
-        elif self.city in Tier_2:
-            return 2
-        else:
-            return 3
-        
-        
+ #created a route for Homepage(Human)
+@app.get('/')
+def Home():
+     return "Insurance Prediction Application"
+ 
+ #To read by machine
+@app.get('/health')
+def health_check():
+    return {
+        'status':'Ok',
+        'version':MODEL_VERSION,
+        'model_loaded':model is not None
+    }
  #Creating a route
-@app.post('/predict') 
+@app.post('/predict',response_model=PredictionResponse) 
 def predict_Insurance(data:User):
     
-    Input_df= pd.DataFrame([{
+    user_input= {
         'bmi':data.bmi,
         'age_group':data.age_group,
         'lifestyle_risk':data.lifestyle_risk,
         'city_tier':data.city_tier,
         'income_lpa':data.income_lpa,
         'occupation':data.occupation    
-    }])
-    #Make prediction 
-    prediction = model.predict(Input_df)[0]
-
-    return {
-    "response": {
-        "predicted_category": str(prediction)
-       }
     }
+    
+    #Make prediction 
+    try:
+        prediction = predict_output(user_input)
+        return JSONResponse(status_code=200,content={"response":prediction})
+    except Exception as e:         
+        return JSONResponse(status_code=500,content=str(e))
